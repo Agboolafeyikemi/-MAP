57d86c447cac2d1725d637b1731c5d5b1bfc4340
Agboola Feyikemi <agboolafeyikemi@gmail.com>
2022-08-09 23:20:04 +0100

    Genesis Commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/// <reference types="@types/google.maps" />
+import { CustomMAP } from "./CustomMap";
+new CustomMAP("map");
+// import { User } from "./Users";
+// import { Company } from "./Company";
+
+// const user = new User();
+// console.log("user", user);
+
+// const company = new Company();
+// console.log("company", company);
